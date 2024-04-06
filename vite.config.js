009163085278758b2d63701e544652cdb63b0a32
yash jangid <yash@ith.tech>
2024-04-06 12:39:38 +0530

    merge

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    rollupOptions: {
+      external: ['@fortawesome/free-solid-svg-icons']
+    }
+  }
 })
